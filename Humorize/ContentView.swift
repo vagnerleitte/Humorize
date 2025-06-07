cbd88dc7df49f101a7fe8e7d09f3b9807c1a83a1
Vagner Leite da Silva <vagner.leite.silva@gmail.com>
2025-06-07 18:23:02 -0300

    Initial Commit

---
+++ Humorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Humorize
+//
+//  Created by Vagner Leite da Silva on 07/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Humorize/HumorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumorizeApp.swift
+//  Humorize
+//
+//  Created by Vagner Leite da Silva on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HumorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
